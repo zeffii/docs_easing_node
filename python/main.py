d09d5fb4db41ef8c674fb8cee0c333a4e1a7d262
zeffii <tetha.z@gmail.com>
2020-04-29 15:16:05 +0200

    local test

--- python/main.py
+++ python/main.py
@@ -1,7 +1,8 @@
 from browser import document
+from sv_easing_functions import easing_dict
 
 document <= "The graphs below represent the easing functions implemented in the Easing node"
 
-from sv_easing_functions import easing_dict
 
 document <= easing_dict[0].__name__
+# print(easing_dict[0].__name__)
