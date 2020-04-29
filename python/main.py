5891be3ccec0e5bc407ac71801fb060d8772fdb6
zeffii <tetha.z@gmail.com>
2020-04-29 15:22:42 +0200

    try this..

--- python/main.py
+++ python/main.py
@@ -1,8 +1,8 @@
 from browser import document
-from sv_easing_functions import easing_dict
+import sv_easing_functions
 
 document <= "The graphs below represent the easing functions implemented in the Easing node"
 
 
-document <= easing_dict[0].__name__
+document <= sv_easing_functions.easing_dict[0].__name__
 # print(easing_dict[0].__name__)
