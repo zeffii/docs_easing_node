62fb9c398c37b2244bd1460d7fde3b6264875b75
zeffii <tetha.z@gmail.com>
2020-04-29 18:02:35 +0200

    test dynamically made paths

--- python/main.py
+++ python/main.py
@@ -2,27 +2,32 @@ from browser import document, svg, html
 import sv_easing_functions
 
 document <= "The graphs below represent the easing functions implemented in the Easing node"
+deltas = [i/40 for i in range(40)]
 
-
-# document <= sv_easing_functions.easing_dict[0].__name__
-# print(easing_dict[0].__name__)
 def get_points(name):
-    if name == "star":
-        return "M 75,38 90,80 135,80 98,107 111,150 75,125 38,150 51,107 15,80 60,80 Z"
+    scale = 150
+    func = getattr(sv_easing_functions, name)
+    results = [(int(delta*scale), int(func(delta)*scale)) for delta in deltas]
 
+    format_item = lambda r: f"{r[0]},{r[1]}"
+    return "M " + " ".join([format_item(r) for r in results]) + " Z"
 
-svg_tag = html.SVG(
-    xmlns="http://www.w3.org/2000/svg",
-    width="200", height="200",
-    style={"border-style": "solid", "border-width":1, "border-color": "#120"})
-svg_tag <= svg.g(id="panel2")
+for idx, easing_func in sv_easing_functions.easing_dict.items():
+    if idx > 4:
+        break
+    easing_name = easing_func.__name__
 
-document <= svg_tag
+    svg_tag = html.SVG(
+        xmlns="http://www.w3.org/2000/svg",
+        width="200", height="200",
+        style={"border-style": "solid", "border-width":1, "border-color": "#120"})
+    svg_tag <= svg.g(id=easing_name)
+    document <= svg_tag
 
-title = svg.text('Title', x=70, y=25, font_size=22, text_anchor="middle")
-# circle = svg.circle(cx=70, cy=120, r=40, stroke="black",stroke_width="2",fill="red")
-path_1 = svg.path(fill="red", stroke="blue", stroke_width="10", d=get_points("star"))
-panel = document["panel2"]
-panel <= title
-panel <= path_1
+    title = svg.text(easing_name, x=70, y=25, font_size=12, text_anchor="middle")
+    path_1 = svg.path(fill="red", stroke="blue", stroke_width="10", d=get_points(easing_name))
+
+    panel = document[easing_name]
+    panel <= title
+    panel <= path_1
 
