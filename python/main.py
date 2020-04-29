b6f97e3370527acec2ef7ac62c4be921cac9127b
zeffii <tetha.z@gmail.com>
2020-04-29 15:35:14 +0200

    use dq instead

--- python/main.py
+++ python/main.py
@@ -10,6 +10,6 @@ document <= sv_easing_functions.easing_dict[0].__name__
 
 title = svg.text('Title', x=70, y=25, font_size=22, text_anchor="middle")
 circle = svg.circle(cx=70, cy=120, r=40, stroke="black",stroke_width="2",fill="red")
-panel = document['panel']
+panel = document["panel"]
 panel <= title
 panel <= circle
