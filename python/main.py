f0b507054b27ad8ebd4316959e66c356d211a097
zeffii <tetha.z@gmail.com>
2020-04-30 14:43:50 +0200

    add optimize points

--- python/main.py
+++ python/main.py
@@ -2,17 +2,22 @@ from browser import document, svg, html
 import sv_easing_functions
 
 # document <= "The graphs below represent the easing functions implemented in the Easing node"
-num_points = 80
-deltas = [i/num_points for i in range(num_points)]
+num_points_high = 80
+num_points_low = 20
+deltas_high = [i/num_points_high for i in range(num_points_high)]
+deltas_low = [i/num_points_low for i in range(num_points_low)]
+
 scale = 150
 
 path_y_offset = 210
 rect_y_offset = path_y_offset - scale 
 g_x_offset, g_y_offset = 25, 0
 
-def get_points(name):
+def get_points(name, idx):
+    """ some functions only need few points so we speed up this function by switching after 21 """
     func = getattr(sv_easing_functions, name)
-    results = [(int(delta*scale), int(func(delta)*-scale)) for delta in deltas]
+    points = deltas_high if idx > 21 else deltas_low 
+    results = [(int(delta*scale), int(func(delta)*-scale)) for delta in points]
     return "M " + " ".join([f"{r[0]},{r[1]}" for r in results])
 
 for idx, easing_func in sv_easing_functions.easing_dict.items():
@@ -29,7 +34,7 @@ for idx, easing_func in sv_easing_functions.easing_dict.items():
     bg = svg.rect(x=0, y=rect_y_offset, width=150, height=150, stroke="#999", fill="#cdcdcd")
     path = svg.path(
         transform=f"translate(0, {path_y_offset})", fill="none", stroke="#333", 
-        stroke_width="2", d=get_points(easing_name))
+        stroke_width="2", d=get_points(easing_name, idx))
     panel = document[easing_name]
     panel <= title
     panel <= bg
